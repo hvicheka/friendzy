22fc536d6dfbc35167b5effaca0586f6e1e14eab
Hok Vicheka <hvicheka.it@gmail.com>
2022-05-20 22:12:01 +0700

    layout

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div class="bg-green-500 text-white text-md">
+      <nav class="container flex justify-between items-center max-w-6xl mx-auto py-5 sm:px-6 lg:px-8">
+        <span class="font-semibold text-2xl">Friendzy</span>
+        <div class="space-x-2">
+          <template>
+            <span>Welcome,
+              <NuxtLink to="#" class="capitalize hover:text-green-200">
+                Vicheka
+              </NuxtLink>
+            </span>
+            <NuxtLink to="#" class="hover:text-green-200">
+              FRIENDS
+            </NuxtLink>
+            <NuxtLink to="#" class="hover:text-green-200">
+              USERS
+            </NuxtLink>
+            <NuxtLink to="/dashboard" class="hover:text-green-200">
+              DASHBOARD
+            </NuxtLink>
+            <NuxtLink to="#" class="hover:text-green-200">
+              REGISTER
+            </NuxtLink>
+            <NuxtLink to="#" class="hover:text-green-200">
+              LOGIN
+            </NuxtLink>
+          </template>
+        </div>
+      </nav>
+    </div>
+    <Nuxt/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "default"
+}
+</script>
+
+<style scoped>
+
+</style>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="container max-w-6xl mx-auto  mt-48 sm:px-6 lg:px-8">
     <div class="flex justify-center items-center bg-green-200 h-56">
-      <span class="text-gray-700 text-5xl">Friendzy</span>
+      <span class="text-gray-700 text-5xl">Dashboard</span>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
-  layout: 'default',
+  name: "dashboard"
 }
 </script>
+
+<style scoped>
+
+</style>
